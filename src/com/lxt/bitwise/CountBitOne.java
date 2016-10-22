16ee2d94b1d7582bc168a7bd95e99d606b83429c
zer0Black <lxtalx@gmail.com>
2016-10-22 22:38:33 +0800

    【添加】整数的二进制表达中1的个数

---
+++ src/com/lxt/bitwise/CountBitOne.java
@@ -0,0 +1,27 @@
+package com.lxt.bitwise;
+
+/**
+ * 整数的二进制表达中1的个数
+ * @author lixuetao
+ *
+ */
+public class CountBitOne {
+
+	public static int count1(int num){
+		int res = 0;
+		while(num != 0){
+			res += num & 1;
+			num >>= 1;
+		}
+		return res;
+	}
+	
+	public static int count2(int num){
+		int res = 0;
+		while(num != 0){
+			num = num & (num-1);
+			res++;
+		}
+		return res;
+	}
+}
